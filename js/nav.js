20ed5fcb649f3a8351d6aa974c65cf07712d2fb9
floroz <tortora87@gmail.com>
2019-06-01 11:40:01 +0100

    fix bugs and modal

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,16 @@
 const navToggle = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
+const containerAll = document.querySelector('.container-all');
+const contAllStyles = containerAll.styles;
+const bodyClassList = document.body.classList;
 
 navToggle.addEventListener('click', () => {
-	document.body.classList.toggle('nav-is-open');
+	contAllStyles.transition = 'all .25s ease-out';
+
+	bodyClassList.toggle('nav-is-open');
+});
+
+nav.addEventListener('click', () => {
+	contAllStyles.transition = '0ms';
+	bodyClassList.remove('nav-is-open');
 });
